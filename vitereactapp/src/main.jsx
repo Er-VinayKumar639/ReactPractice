86bd3cad58edcdeac5ee40f7270c41cf71404c97
Er-VinayKumar639 <123vinaychaurasiya@gmail.com>
2024-01-31 03:11:56 +0530

    basic react and vite app created

--- vitereactapp/src/main.jsx
+++ vitereactapp/src/main.jsx
@@ -1,10 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './index.css'
+let username = 'VINAY KUMAR'
+// const reactElement ={
+//     type: 'a' , 
+//     props: {
+//       href: "https://www.google.com",
+//       target: '_blank',
+//     },
+//     children: 'Click me to visit google'
+//   };
 
+const AreactElement = React.createElement(
+    'a',
+    {href: 'https://google.com', target: '_blank'},
+    username
+)
+
+function MyApp(){
+    return(
+        <div>
+            <h1>Custom React App</h1>
+            {AreactElement}
+            {/* {reactElement}  // Not Working */}
+        </div>    
+    )
+}  
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  
+    <MyApp />
+    
+ 
 )
